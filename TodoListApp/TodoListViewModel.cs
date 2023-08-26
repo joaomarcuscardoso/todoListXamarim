bf3c6db9f93f7677e1e9ec19db0281f7fc34a64e
joaomarcuscardoso <rfcjoujou@gmail.com>
2023-08-26 20:58:57 -0300

    Finalizando o App[Add, Delete, Update] the checkbox

--- TodoListApp/TodoListViewModel.cs
+++ TodoListApp/TodoListViewModel.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace TodoListApp
 {
@@ -13,9 +15,28 @@ namespace TodoListApp
         {
             TodoItems = new ObservableCollection<TodoItem>();
 
-            TodoItems.Add(new TodoItem("todo 1", false));
+            TodoItems.Add(new TodoItem("todo 1", true));
             TodoItems.Add(new TodoItem("todo 2", false));
             TodoItems.Add(new TodoItem("todo 3", false));
         }
+
+
+        public ICommand AddTodoCommand => new Command(AddTodoItem);
+        public string NewTodoInputValue { get; set; }
+
+        void AddTodoItem()
+        {
+            TodoItems.Add(new TodoItem(NewTodoInputValue, false));
+        }
+
+        public ICommand RemoveTodoCommand => new Command(RemoveTodoItem);
+
+        void RemoveTodoItem(object o)
+        {
+            TodoItem todoItemBeignRemoved = o as TodoItem;
+            TodoItems.Remove(todoItemBeignRemoved);
+            
+        }
+
     }
 }
